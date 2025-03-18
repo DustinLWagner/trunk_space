38041ab13a2415818794044ceabaf037158a8856
DustinLWagner <DustinWagner@gmail.com>
2025-03-18 12:51:56 -0500

    proj-01, changed info panel to h1"revealCoin". create function to change h1"revealCoin" innerText. reorderd script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-function flipCoin() {
-
-    console.log("You flipped the coin!");
-
-}
-
-//get element
-const coin = document.getElementById("coinFlip");
+//get elements
+const coin = document.getElementById("headsCoin");
 
 //add listener
 coin.addEventListener("click", flipCoin);
+coin.addEventListener("click", flipResult);
+
+//this logs to console that coin is clicked
+// Change this to only calculate the coin flip.
+function flipCoin() {
+
+    console.log("You flipped the coin!");
+};
+
+//this modifies h1"revealCoin" to display coin flip result
+// Alter this to display results and log to console.
+function flipResult() {
+    revealCoin.innerText = "Heads"
+};
