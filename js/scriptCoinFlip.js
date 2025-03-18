f613ef0dd758659dca3403e5d84b8818749bc193
DustinLWagner <DustinWagner@gmail.com>
2025-03-18 15:46:29 -0500

    Fixed revealCoin const, added +1 to math.random * max, fixed heads or tails output to h1"reveal coin" now working properly, passed randint through to flipResult function properly.

--- js/scriptCoinFlip.js
+++ js/scriptCoinFlip.js
@@ -1,34 +1,33 @@
-const heads = (1, 3, 5, 7, 9);
-const tails = (0, 2, 4, 6, 8);
-
-
 //get elements
 const coin = document.getElementById("headsCoin");
-
-//add listeners
-coin.addEventListener("click", flipCoin);
-coin.addEventListener("click", flipResult);
+const revealCoin = document.getElementById("revealCoin");
 
 //get a random integer 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-
+    return Math.floor(Math.random() * max + 1);
 }
+
+//add listeners
+coin.addEventListener("click", flipCoin);
+
 
 //this logs to console that coin is clicked and logs the number
 function flipCoin() {
-    console.log(getRandomInt(10));
     console.log("You flipped the coin!");
+    let randInt = getRandomInt(2);
+    flipResult(randInt);
+    console.log(randInt);
 };
 
 
 
 //this modifies h1"revealCoin" to display coin flip result
-function flipResult() {
-    if (getRandomInt = heads) {
+
+function flipResult(randInt) {
+    if (randInt === 1) {
         revealCoin.innerText = "Heads"
     }
-    else if (getRandomInt = tails) {
+    else if (randInt === 2) {
         revealCoin.innerText = "Tails"
     }
     else {
