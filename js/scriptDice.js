0934130b89414788188115f5a9cfea5e837bd366
DustinLWagner <DustinWagner@gmail.com>
2025-03-19 20:33:39 -0500

    condensed getElement and EventListeners for dice.

--- js/scriptDice.js
+++ js/scriptDice.js
@@ -1,18 +1,18 @@
-//create varibles for get elements//
-const diceD4 = document.getElementById("diceD4");
-const diceD6 = document.getElementById("diceD6");
-const diceD8 = document.getElementById("diceD8");
-const diceD10 = document.getElementById("diceD10");
-const diceD12 = document.getElementById("diceD12");
-const diceD20 = document.getElementById("diceD20");
+//create varibles, get elements, listen for click, alert to console//
+const diceD4 = document.getElementById("diceD4").addEventListener("click", alert);
+const diceD6 = document.getElementById("diceD6").addEventListener("click", alert);
+const diceD8 = document.getElementById("diceD8").addEventListener("click", alert);
+const diceD10 = document.getElementById("diceD10").addEventListener("click", alert);
+const diceD12 = document.getElementById("diceD12").addEventListener("click", alert);
+const diceD20 = document.getElementById("diceD20").addEventListener("click", alert);
 
 //add listeners//
-diceD4.addEventListener("click", alert);
-diceD6.addEventListener("click", alert);
-diceD8.addEventListener("click", alert);
-diceD10.addEventListener("click", alert);
-diceD12.addEventListener("click", alert);
-diceD20.addEventListener("click", alert);
+//diceD4.addEventListener("click", alert);
+//diceD6.addEventListener("click", alert);
+//diceD8.addEventListener("click", alert);
+//diceD10.addEventListener("click", alert);
+//diceD12.addEventListener("click", alert);
+//diceD20.addEventListener("click", alert);
 
 //get random integers//
 
