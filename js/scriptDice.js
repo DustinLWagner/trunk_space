1544753c524cfbc64c386dec95d5ec8baf6a83b7
DustinLWagner <DustinWagner@gmail.com>
2025-03-24 16:03:13 -0500

    refactored dice roll listeners into a dynamic listener, rotateDice function we worked into a single 160deg spin. Still working towards repeatable click and spin.

--- js/scriptDice.js
+++ js/scriptDice.js
@@ -1,14 +1,3 @@
-/* OLD
-create varibles, get elements, listen for click, alert to console//
-const diceD4 = document.getElementById("diceD4").addEventListener("click", rollD4);
-const diceD6 = document.getElementById("diceD6").addEventListener("click", rollD6);
-const diceD8 = document.getElementById("diceD8").addEventListener("click", rollD8);
-const diceD10 = document.getElementById("diceD10").addEventListener("click", rollD10);
-const diceD12 = document.getElementById("diceD12").addEventListener("click", rollD12);
-const diceD20 = document.getElementById("diceD20").addEventListener("click", rollD20);
-*/
-const spinTime = 100;
-
 //array to store dice ids//
 const diceIds = ["4", "6", "8", "10", "12", "20"]
 
@@ -17,69 +6,40 @@ diceIds.forEach(diceId => {
     document.getElementById(diceId).addEventListener("click", function () {
         let randInt = Math.floor(Math.random() * diceId + 1);
         console.log(window.event.target.id);
-        console.log("You rolled a " + randInt);
-        diceResult.innerText = ("You rolled a " + randInt);
+        console.log("You rolled " + randInt);
+        diceResult.innerText = ("You rolled " + randInt);
     });
 });
 
-
-/*
-//get random integer//
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max + 1);
-};
-
-// old roll the dice//
-
-function rollD4() {
-    let randInt = getRandomInt(4);
-    console.log(window.event.target.id);
-    console.log("You rolled a " + randInt);
-    diceResult.innerText = ("You rolled a " + randInt);
-};
-function rollD6() {
-    let randInt = getRandomInt(6);
-    console.log(window.event.target.id);
-    console.log("You rolled a " + randInt);
-    diceResult.innerText = ("You rolled a " + randInt);
-};
-function rollD8() {
-    let randInt = getRandomInt(8);
-    console.log(window.event.target.id);
-    console.log("You rolled a " + randInt);
-    diceResult.innerText = ("You rolled a " + randInt);
-};
-function rollD10() {
-    let randInt = getRandomInt(10);
-    console.log(window.event.target.id);
-    console.log("You rolled a " + randInt);
-    diceResult.innerText = ("You rolled a " + randInt);
-};
-function rollD12() {
-    let randInt = getRandomInt(12);
-    console.log(window.event.target.id);
-    console.log("You rolled a " + randInt);
-    diceResult.innerText = ("You rolled a " + randInt);
-};
-function rollD20() {
-    let randInt = getRandomInt(20);
-    console.log(window.event.target.id);
-    console.log("You rolled a " + randInt);
-    diceResult.innerText = ("You rolled a " + randInt);
-};
-*/
+//create vars and arrays for roll animation
+const rotations = [90, 180, 270, 360];
 
 // HTMLclass diceRolled animation needed //
 
+diceIds.forEach(diceId => {
+    document.getElementById(diceId).addEventListener("click", function () {
+        rotateDice(this, 1000);
+    })
+});
+
+
+function rotateDice(el, spinTime) {
+    setTimeout(() => {
+        el.classList.add('rotateable');
+        el.style.transform = `rotate(160deg)`;
+        el.style.transition = `transform ${spinTime}ms`;
+    }, 0);
+
+};
 
 
 
 
+/*
 // terribly rotate the dice on click //
 function rotateDice(el) {
     el.classList.add('rotateable');
-    el.style.transform = 'rotate45deg)';
+    el.style.transform = 'rotations';
     setTimeout(function () {
         el.style.transform = 'rotate(90deg)';
         setTimeout(function () {
@@ -108,6 +68,7 @@ function rotateDice(el) {
 
 }
 
+
 document.getElementById("4").addEventListener("click", function (ev) {
     rotateDice(this);
 })
@@ -125,4 +86,4 @@ document.getElementById("12").addEventListener("click", function (ev) {
 })
 document.getElementById("20").addEventListener("click", function (ev) {
     rotateDice(this);
-})
+})*/
