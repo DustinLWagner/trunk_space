0ecf0fce781fdd676c78184378855761b59d4ad1
DustinLWagner <DustinWagner@gmail.com>
2025-03-26 14:32:15 -0500

    animate the D4 dice using CSS keyframes, disable js  dice rotation

--- js/scriptDice.js
+++ js/scriptDice.js
@@ -1,6 +1,6 @@
 //array to store dice ids//
-const diceIds = ["4", "6", "8", "10", "12", "20"]
-//use forEach to add listenters to all of them and roll
+const diceIds = ["4", "6", "8", "10", "12", "20"];
+//use forEach to add listenters to all dice and roll
 diceIds.forEach(diceId => {
     document.getElementById(diceId).addEventListener("click", function () {
         let randInt = Math.floor(Math.random() * diceId + 1);
@@ -9,16 +9,31 @@ diceIds.forEach(diceId => {
         diceResult.innerText = ("You rolled " + randInt);
     });
 });
+
+
+const image4 = document.getElementById('4');
+
+image4.addEventListener('click', () => {
+    image4.classList.add('rotateDice');
+});
+
+
+
 //create vars and arrays for roll animation
-const rotations = [90, 180, 270, 360];
-const resetDice = (3000);
+//const rotations = [90, 180, 270, 360];
+//const resetDice = (3000);
+
+/*
 //use forEach to rotate each diceId
 diceIds.forEach(diceId => {
     document.getElementById(diceId).addEventListener("click", function () {
-        rotateDice(this, 1000);
+        diceId.classList.add('rotateDice');
     })
 });
+
+
 // rotateDice animation  //
+
 function rotateDice(el, spinTime) {
 
     el.classList.add('rotateable');
@@ -33,3 +48,4 @@ function rotateDice(el, spinTime) {
         el.style.transform = `rotate(0deg)`;
     }, resetDice);
 };
+*/
