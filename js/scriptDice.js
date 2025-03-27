27cabd3576cc4007bb0e9ec0b0b3c5ccc94c5d8c
DustinLWagner <DustinWagner@gmail.com>
2025-03-27 11:42:07 -0500

    removed Past Rolls text from diceRolls array, moved console.log diceRolls to diceResult 2sec timeout, changed let randInt > var randInt, changed diceRolls.push results.innerText to randInt + diceId, changed belowDiceResult div from class > ID, added diceRollsBox ID <P> to belowDiceResult, store and display diceRolls inside diceRollsBox

--- js/scriptDice.js
+++ js/scriptDice.js
@@ -1,10 +1,10 @@
 //arrays to store dice ids and past rolls//
 const diceIds = ["4", "6", "8", "10", "12", "20"];
-var diceRolls = ["Past Rolls"];
+var diceRolls = [];
 //use forEach to add listenters and grab el Id for all dice and roll dice
 diceIds.forEach(diceId => {
     document.getElementById(diceId).addEventListener("click", function () {
-        let randInt = Math.floor(Math.random() * diceId + 1);
+        var randInt = Math.floor(Math.random() * diceId + 1);
         document.getElementById(diceId).classList.add('rotateDice');
         //logs diceId to console with "you rolled randInt'
         console.log(window.event.target.id);
@@ -16,12 +16,13 @@ diceIds.forEach(diceId => {
         setTimeout(() => {
             diceResult.innerText = ("You rolled " + randInt + " on a D" + diceId);
             //store dice result in dice roll array as its created and before it is cleared
-            diceRolls.push(diceResult.innerText);
+            diceRolls.push(" " + randInt + " on a D" + diceId);
+            //log dice rolls array
+            console.log(diceRolls);
+            diceRollsBox.innerText = (diceRolls);
         }, 2000);
         setTimeout(() => {
             diceResult.innerText = (" ");
-            //log dice rolls array
-            console.log(diceRolls);
         }, 5000);
 
     });
