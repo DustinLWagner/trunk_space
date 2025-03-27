a224463039ddae3810e1701bed3215e80e2566fb
DustinLWagner <DustinWagner@gmail.com>
2025-03-27 11:12:44 -0500

    added id diceRolls to h1 of belowDiceResult, created diceRolls var array, push diceresult.innerText to diceRolls array, log diceRolls to console,

--- js/scriptDice.js
+++ js/scriptDice.js
@@ -1,10 +1,12 @@
-//array to store dice ids//
+//arrays to store dice ids and past rolls//
 const diceIds = ["4", "6", "8", "10", "12", "20"];
+var diceRolls = ["Past Rolls"];
 //use forEach to add listenters and grab el Id for all dice and roll dice
 diceIds.forEach(diceId => {
     document.getElementById(diceId).addEventListener("click", function () {
         let randInt = Math.floor(Math.random() * diceId + 1);
         document.getElementById(diceId).classList.add('rotateDice');
+        //logs diceId to console with "you rolled randInt'
         console.log(window.event.target.id);
         console.log("You rolled " + randInt);
         //time outs to reset the dice, display dice result, and remove it
@@ -13,13 +15,24 @@ diceIds.forEach(diceId => {
         }, 3000);
         setTimeout(() => {
             diceResult.innerText = ("You rolled " + randInt + " on a D" + diceId);
+            //store dice result in dice roll array as its created and before it is cleared
+            diceRolls.push(diceResult.innerText);
         }, 2000);
         setTimeout(() => {
             diceResult.innerText = (" ");
+            //log dice rolls array
+            console.log(diceRolls);
         }, 5000);
+
     });
 });
 
 /*
-create an array to store the dice values and display the values history in a div 
+create an array to store the dice values and display the values history in a div
 */
+
+//function storeRoll(diceRolls) {}
+
+//document.getElementById(diceRolls)
+
+//diceRolls.innerText = (diceRolls);
